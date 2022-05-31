179bf2a2977359939d750a89eca384cc0339864c
MarcalDev <marcaldesenvolvedor@gmail.com>
2022-05-30 21:52:41 -0300

    Primeiro commit

---
+++ AppNetflix/AppNetflix/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Helvetica.ttf", Alias = "Helvetica")] 
+[assembly: ExportFont("Helvetica-Bold.ttf", Alias = "HelveticaBold")] 
+[assembly: ExportFont("Helvetica-Light.ttf", Alias = "HelveticaLight")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "MontserratBold")] 
